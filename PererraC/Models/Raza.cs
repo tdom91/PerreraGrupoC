6d8c707709857acbcfce05e337f10e68d3cdb9cf
Francisco Lorente <francislorenruiz@outlook.es>
2020-02-18 14:24:43 +0100

    Añadidos los modelos de jaula y raza.

--- PererraC/Models/Raza.cs
+++ PererraC/Models/Raza.cs
@@ -7,5 +7,9 @@ namespace PererraC.Models
 {
     public class Raza
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public virtual ICollection<Perros> Perros { get; set; }
     }
 }
