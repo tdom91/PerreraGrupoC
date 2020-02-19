da76848a5c2d5e7e7589229f8acb00cfe1b5f9a2
aleblasco39 <aleblasco77@gmail.com>
2020-02-19 09:01:22 +0100

    Añadido modelo Empleado

--- PererraC/Models/Empleado.cs
+++ PererraC/Models/Empleado.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,17 @@ namespace PererraC.Models
 {
     public class Empleado
     {
+        private int IdEmpleado { get; set; }
+
+        private string NombreEmpleado{ get; set; }
+
+        private string TelefonoEmpleado { get; set; }
+
+        private string CorreoEmpleado { get; set; }
+            
+        private string DNIEmpleado { get; set; }
+        
+        public virtual ICollection<Adopcion> Adopciones { get; set; }
+
     }
 }
