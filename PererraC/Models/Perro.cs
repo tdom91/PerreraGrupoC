d163b4f593b94b6842d36bffa9a58e7ea6e10e01
Alberto Martinez <martini73.aml@gmail.com>
2020-02-19 08:38:38 +0100

    Perros Model

--- PererraC/Models/Perro.cs
+++ PererraC/Models/Perro.cs
@@ -7,5 +7,16 @@ namespace PererraC.Models
 {
     public class Perro
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Chip { get; set; }
+        public Nullable<System.DateTime> FechaNacimiento { get; set; }
+        public Nullable<int> CodRazaId { get; set; }
+        public Nullable<int> IdJaula { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Adopcion> Adopciones { get; set; }
+        public virtual Jaula Jaulas { get; set; }
+        public virtual Raza Razas { get; set; }
     }
 }
